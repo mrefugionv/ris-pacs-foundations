8363a6a70cc7df697463597a5fa19e443f65a769
Refugio Noriega <mrefugionoriegav@gmail.com>
2026-05-22 13:28:54 -0600

    Update 5_ris-pacs_integration_ERdiagram.py
    Updating ER diagram

--- 5_ris-pacs_integration_ERdiagram.py
+++ 5_ris-pacs_integration_ERdiagram.py
@@ -1,93 +1,95 @@
 #  This code defines the database structure of the 
 # Entity-Relationship Diagram presented on "5_ris_pacs_integration.md"
 # Created on  https://dbml.dbdiagram.io/docs
+# https://dbdiagram.io/d
 
-Table patient {
+
+Table patients {
   patient_id integer [primary key] 
-  medical_record_number verchar [note:'unique']
-  first_name varchar
-  last_name varchar
+  medical_record_number varchar(50) [note:'unique']
+  first_name varchar(30)
+  last_name varchar(30)
   date_of_birth datetime
-  gender char
-  contact_info varchar [note:'or json']
-  primary_physician_id integer [ref: > physician.physician_id, unique]
-  insurance_id varchar
-  status enum [note: 'active/inactive /deceased']
+  gender varchar(15) [note:'default value: Not Specified']
+  contact_info varchar(45) [note:'or json']
+  primary_physician_id integer [ref: > physicians.physician_id, unique]
+  insurance_id integer [note:'UN, Zerofill']
+  patient_status enum [note: 'active/inactive /deceased']
 }
 
-Table study {
+Table studies {
   study_id integer [primary key]
-  patient_id integer [ref: > patient.patient_id, unique]
-  request_id integer
-  accession_number varchar [note:'unique']
+  patient_id integer 
+  request_id integer [ref: > order_requests.request_id, unique]
+  accession_number varchar(45) [note:'unique']
   study_datetime datetime
-  modality_id integer
-  body_part varchar
-  status enum [note: 'scheduled / in_progress / completed / canceled']
-  referring_physician_id integer [ref: > physician.physician_id, unique]
-  performing_tecnologist_id varchar
+  equipment_id integer [ref: > modalities.equipment_id, unique]
+  body_part varchar(45)
+  study_status enum [note: 'scheduled / in_progress / completed / canceled']
+  ordering_physician_id integer [ref: > physicians.physician_id, unique]
+  performing_technician_id integer [ref: > physicians.physician_id, unique]
 }
 
-Table report {
+Table reports {
   report_id integer [primary key]
-  study_id integer [ref: > study.study_id, unique]
-  report_datetime datetime
-  radiologist_id integer [ref: > physician.physician_id, unique]
-  findings text
-  impression text
-  status enum [note:' draft / final / amended / canceled']
+  study_id integer [ref: > studies.study_id, unique]
   signed_datetime datetime
-  distribution_list text [note:'or json']
+  radiologist_id integer [ref: > physicians.physician_id, unique]
+  findings_text text
+  report_status enum [note:' draft / final / amended / canceled']
+  distribution_list varcgar(150) [note:'or json']
 }
 
-Table order_request{
+Table order_requests{
   request_id integer [primary key]
-  patient_id integer [ref: > patient.patient_id, unique]
-  ordering_physician_id integer [ref: > physician.physician_id, unique]
-  requested_procedure_code varchar
-  clinical_indication text
+  patient_id integer [ref: > patients.patient_id, unique]
+  ordering_physician_id integer [ref: > physicians.physician_id, unique]
+  requested_procedure_code varchar(20)
+  clinical_indication text(150)
   priority enum [note:' routine /urgent / stat']
   request_datetime datetime
   status enum [note:'oredered / scheduled / completed / canceled']
 }
 
-Table scheduled_appointment{
-  apointment_id integer [primary key]
-  requested_id  integer [ref: > order_request.request_id, unique]
-  modality_id integer [ref: > modality.modality_id, unique]
+Table scheduled_appointments{
+  appointment_id integer [primary key]
+  request_id  integer [ref: > order_requests.request_id, unique]
+  equipment_id integer [ref: > modalities.equipment_id, unique]
   scheduled_start datetime
   scheduled_end datetime
   room varchar
-  performing_technologist_id integer [ref: > physician.physician_id, unique]
-  status enum [note:'booked / arrived / no-show / completed']
+  performing_technician_id integer [ref: > physicians.physician_id, unique]
+  appointment_status enum [note:'booked / arrived / no-show / completed']
 }
 
-Table modality {
-  modality_id integer [primary key]
+Table modalities {
+  equipment_id integer [primary key]
   modality_type enum [note:'CT / MR / US /CR/ MG']
-  ae_title varchar
-  ip_address varchar
-  port integer
-  location varchar
-  manufacturer varchar
-  model varchar
+  ae_title varchar(30)
+  ip_address varchar(15)
+  modality_port integer
+  location varchar(45)
+  manufacturer varchar(20)
+  model varchar(15)
 }
 
-Table physician {
+Table physicians {
   physician_id  integer [note:'unique']
-  physician_name varchar
-  speciality varchar
-  role enum (' ordering / refering / radiologist / technician')
-  contact_info varchar [note:'or json']
-  license_number varchar
+  physician_name varchar(100)
+  speciality varchar(20)
+  date_of_birth datetime
+  room varchar (20)
+  contact_info varchar(50) [note:'or json']
+  license_number varchar(30)
+  physician_status enum [note:'active /inactive/deceased']
 }
 
 Table audit_log {
   log_id integer [primary key]
   user_id integer
-  action enum [note: 'create / update / delete/ login']
+  log_action enum [note: 'insert / update / delete']
   entity_type enum [note: ' patient / report / order_request / scheduled_appointment/ physician / modality']
-  entity_id integer
+  entity_id integer 
   datetime timestamp
   previous_value text
   new_value text
